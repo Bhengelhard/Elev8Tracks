9bd6cb187c637cea56379990f9ecf41dae675f43
pmoot4 <pmoot4@gmail.com>
2015-10-18 13:39:34 -0400

    test

--- controllers/playlistC.js
+++ controllers/playlistC.js
@@ -120,7 +120,7 @@ exports.videoSearch = function(req, res) {
 	sql = sql.substring(0, sql.length - 3) + ')';
 	sql2 = sql2.substring(0, sql2.length - 3) + ')';
 	var filter = req.body.filterParams.split(',');
-	console.log(filter);
+	console.log(req.body.sortParams);
 	if(filter[0].length > 0) {
 		for(var j = 0; j < filter.length; j++) {
 			sql += ' AND ' + filter[j] + "=1";
@@ -129,7 +129,7 @@ exports.videoSearch = function(req, res) {
 	}
 	//where(search.knex.raw(sql)).orWhere(search.knex.raw(sql2)).offset(req.body.offset).limit(req.body.limit)
 	Song.collection().query(function(search) {
-		search.offset(req.body.offset).limit(req.body.limit).orderBy(req.body.sortParams, 'asc');
+		search.where(search.knex.raw(sql)).offset(req.body.offset).limit(req.body.limit).orderBy(req.body.sortParams, 'asc');
 	}).fetch()
 	.then(function(m) {
 		//res.send(m);
