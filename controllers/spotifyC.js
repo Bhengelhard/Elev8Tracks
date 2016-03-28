5416a2ecef71c296054fbe615379325b63c8ab32
pmoot4 <pmoot4@gmail.com>
2016-03-27 20:57:52 -0400

    spotify fix

--- controllers/spotifyC.js
+++ controllers/spotifyC.js
@@ -30,7 +30,16 @@ exports.login = function(req, res) {
   res.cookie(stateKey, state);
   // your application requests authorization
   var scope = 'user-read-private user-read-email playlist-read-private';
-  res.redirect('https://www.google.com/');
+  var url = 'https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state,
+      show_dialog: true
+    });
+  res.send("<script> top.location.href='"+ url + "'</script>");
   // res.redirect('https://accounts.spotify.com/authorize?' +
   //   querystring.stringify({
   //     response_type: 'code',
