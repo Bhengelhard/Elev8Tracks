93399b426f0e4178e0a569b9660248a949fab889
pmoot4 <pmoot4@gmail.com>
2016-05-29 13:44:36 -0400

    spotify data fix

--- controllers/spotifyC.js
+++ controllers/spotifyC.js
@@ -464,7 +464,7 @@ exports.dataUpdate = function(req, res) {
   var queries = [];
   var ids = [];
   var n = 0;
-  Knex('spotify_match').groupByRaw('song_id WITH ROLLUP')
+  Knex('spotify_match').groupBy('song_id','spotify_id')
   .then(function(matches) {
     console.log(matches);
     if(matches.rows) {
