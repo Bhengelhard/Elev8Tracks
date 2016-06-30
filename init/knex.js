20616393c82c2fdbfaff8f471c3ca3098a98028f
pmoot4 <pmoot4@gmail.com>
2016-06-30 07:24:50 -0400

    database switch

--- init/knex.js
+++ init/knex.js
@@ -3,10 +3,10 @@ var Knex = require('knex');
 var knex = Knex.initialize({
 	client: 'postgresql',
 	connection: {
-		host     : 'ec2-174-129-1-179.compute-1.amazonaws.com',
-	    user     : 'imggxboqaxmptt',
-	    password : 'phNpSipbNZtb2MlmdGA_EbmSUM',
-	    database : 'd6d185sef5pibr',
+		host     : 'ec2-54-243-218-37.compute-1.amazonaws.com',
+	    user     : 'ktnkvwfvkbgwnb',
+	    password : 'mmhtn8vNbOggjVcKsm8BJx2Q-A',
+	    database : 'd4mo9hmb7qm1gd',
 	    charset  : 'utf8'
 	}
 });
