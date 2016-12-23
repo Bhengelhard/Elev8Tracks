b8b05254d18df7a971cbc5e2757d8b30d704572e
pmoot4 <pmoot4@gmail.com>
2016-12-23 13:02:52 -0500

    db test

--- init/knex.js
+++ init/knex.js
@@ -1,12 +1,12 @@
 var Knex = require('knex');
-//test
+
 var knex = Knex.initialize({
 	client: 'postgresql',
 	connection: {
-		host     : 'ec2-54-243-218-37.compute-1.amazonaws.com',
-	    user     : 'ktnkvwfvkbgwnb',
-	    password : 'mmhtn8vNbOggjVcKsm8BJx2Q-A',
-	    database : 'd4mo9hmb7qm1gd',
+		host     : 'ec2-174-129-1-179.compute-1.amazonaws.com',
+	    user     : 'imggxboqaxmptt',
+	    password : 'phNpSipbNZtb2MlmdGA_EbmSUM',
+	    database : 'd6d185sef5pibr',
 	    charset  : 'utf8'
 	}
 });
